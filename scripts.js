2f9dbd773390ea8112be8023c7c3e3c92bbc74ed
Bruno17011996 <brusuilis1717@hotmail.com>
2024-10-24 22:28:41 -0300

    nuvem_iphone

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function TrocaCor(cor) {
+  let circulo = document.querySelector(".circulo");
+  circulo.style.background = cor;
+}
+
+function TrocaImagem(imagem) {
+  let imgIphone = document.querySelector(".Iphone");
+  imgIphone.src = imagem;
+}
